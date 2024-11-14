88633070c31681e4e1b168ec89491eb8c57b5823
Itravain <icarotravain@gmail.com>
2024-11-14 14:20:43 -0300

    template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Clique aqui
+      </Button>
+    </div>
+  );
+}
+
+export default App;
